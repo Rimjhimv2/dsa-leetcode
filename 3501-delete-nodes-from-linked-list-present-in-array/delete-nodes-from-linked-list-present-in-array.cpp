b1443d9f4db4ca87a5ca0528b15314364a069d28
Rimjhim Verma <rimjhimv30@gmail.com>
2026-02-16 00:38:32 +0530

    Time: 90 ms (79.21%) | Memory: 263.4 MB (55.97%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,12 +1,14 @@
+
+//classic recursion
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
         
         unordered_set<int> st(begin(nums), end(nums));
 
-        // Remove from beginning
+       
         while(head != NULL && st.find(head->val) != st.end()){
-            head = head->next;   // delete removed
+            head = head->next;   
         }
 
         ListNode* curr = head;
@@ -14,7 +16,7 @@ public:
         while(curr != NULL && curr->next != NULL){
             
             if(st.find(curr->next->val) != st.end()){
-                curr->next = curr->next->next;   // delete removed
+                curr->next = curr->next->next;   
             }
             else{
                 curr = curr->next;
