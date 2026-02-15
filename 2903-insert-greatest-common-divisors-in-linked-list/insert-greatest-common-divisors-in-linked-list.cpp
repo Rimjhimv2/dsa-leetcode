302f820a9e2845084979dd19168953402ba1558c
Rimjhim Verma <rimjhimv30@gmail.com>
2026-02-15 23:32:34 +0530

    Time: 2 ms (79.14%) | Memory: 36.4 MB (4.24%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
@@ -1,47 +1,49 @@
-class Solution {
-public:
-    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+// //iterative approch
 
-        if(!head || !head->next) {
-            return head;
-        }
-
-        ListNode* currNode = head;
-
-        while(currNode && currNode->next) {
-
-            ListNode* nextNode = currNode->next;
-
-            int gcdValue = __gcd(currNode->val, nextNode->val);
-
-            ListNode* temp = new ListNode(gcdValue);
-
-            currNode->next = temp;
-            temp->next = nextNode;
-
-            currNode = nextNode;   // move 2 steps ahead
-        }
-
-        return head;
-    }
-};
-
-
-//  class Solution {
+// class Solution {
 // public:
-//      ListNode* insertGreatestCommonDivisors(ListNode* head) {
-//         if(!head || !head->next){
+//     ListNode* insertGreatestCommonDivisors(ListNode* head) {
+
+//         if(!head || !head->next) {
 //             return head;
 //         }
 
-// ListNode* temp = insertGreatestCommonDivisors(head->next);
+//         ListNode* currNode = head;
 
-// ListNode* gcdNode= new ListNode(__gcd(head->val,head->next->val));
+//         while(currNode && currNode->next) {
 
-// gcdNode->next = temp;
-// head->next = gcdNode;
-// return head;
+//             ListNode* nextNode = currNode->next;
+
+//             int gcdValue = __gcd(currNode->val, nextNode->val);
+
+//             ListNode* temp = new ListNode(gcdValue);
+
+//             currNode->next = temp;
+//             temp->next = nextNode;
+
+//             currNode = nextNode;   // move 2 steps ahead
+//         }
+
+//         return head;
+//     }
+// };
+
+//recursive approch
+ class Solution {
+public:
+     ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        if(!head || !head->next){
+            return head;
+        }
+
+ListNode* temp = insertGreatestCommonDivisors(head->next);
+
+ListNode* gcdNode= new ListNode(__gcd(head->val,head->next->val));
+
+gcdNode->next = temp;
+head->next = gcdNode;
+return head;
 
 
-//      }
-//  };
+     }
+ };
