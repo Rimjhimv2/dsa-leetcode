e9f36dc335d2201f4d21c987a257a66708423a0f
Rimjhim Verma <rimjhimv30@gmail.com>
2026-02-18 21:46:57 +0530

    Time: 941 ms (14.91%) | Memory: 178.8 MB (15.46%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,24 +1,56 @@
+// class Solution {
+// public:
+//     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+
+//         int n = arr.size();
+//         priority_queue<vector<double>>pq;
+//         for(int i = 0 ; i<n-1;i++){
+//             for(int j =0;j<n;j++){
+//                 double div = (double)arr[i]/arr[j];
+//                 pq.push(vector<double>{div,(double)arr[i],(double)arr[j]});
+//                 if(pq.size()>k){
+//                     pq.pop();
+//                 }
+//             }
+            
+//         }
+//         auto rec = pq.top();
+
+//             vector<int>result(2);
+//             result[0]=rec[1];
+//             result[1]= rec[2];
+//             return result ;
+//     }
+// };
+
+// 2nd approch 
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
 
-        int n = arr.size();
-        priority_queue<vector<double>>pq;
-        for(int i = 0 ; i<n-1;i++){
-            for(int j =0;j<n;j++){
-                double div = (double)arr[i]/arr[j];
-                pq.push(vector<double>{div,(double)arr[i],(double)arr[j]});
-                if(pq.size()>k){
-                    pq.pop();
-                }
-            }
-            
-        }
-        auto rec = pq.top();
 
-            vector<int>result(2);
-            result[0]=rec[1];
-            result[1]= rec[2];
-            return result ;
-    }
+        typedef vector<double>V;
+        priority_queue<V,vector<V>,greater<V>>pq;
+        int n = arr.size();
+        for(int i =0;i<n-1;i++){
+          double div = (double)arr[i] / arr[n-1];
+
+            pq.push({div,(double)i,(double)n-1});
+        }
+        int smallest = 1;
+        while(smallest<k){
+            V vec = pq.top();
+            pq.pop();
+            int i = vec[1];
+            int j = vec[2]-1;
+            pq.push({(double)arr[i]/arr[j],(double)i,(double)j});
+            smallest++;
+        }
+        V vec = pq.top();
+        int i = vec[1];
+        int j = vec[2];
+        return {arr[i], arr[j]};
+
+
+         }
 };
