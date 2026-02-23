78a9f5e21499663d05aaf88ce212af8edf3910c5
Rimjhim Verma <rimjhimv30@gmail.com>
2026-02-24 00:07:05 +0530

    Time: 75 ms (78.39%) | Memory: 146.3 MB (81.71%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        //hamne map bnaya 
+        unordered_map<int,int>mp;
+        //0 ko initailze karo 
+        mp[0]=-1;
+        int sum = 0;
+        //sum nikalo 
+        //Har index pe total sum ka remainder nikal rahe.
+        for(int i =0;i<n;i++){
+          sum+= nums[i];
+        
+        // remainder kse nikalega mod nikalo with respect to k 
+         int remainder = sum% k;
+        //hamne sum toh nikal liya ab kya karna hai 
+        //ab map mai check karna 
+        //Agar remainder pehle mil chuka hai:
+        if(mp.find(remainder) != mp.end()){
+            //ab hamne pure map mai dekha 
+            //hame mila toh ab ham current index nikalenge 
+            if(i - mp[remainder]>= 2){
+                return true;
+            }
+        }
+            else{
+                mp[remainder ]=i;
+            }
+        }
+        
+        return false;
+    }
+};
