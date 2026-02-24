22a4aa3767bbbe2825020622dc3cce27a39d689e
Rimjhim Verma <rimjhimv30@gmail.com>
2026-02-24 19:29:49 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (62.84%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,3 +1,21 @@
+
+//imp points to be remembber 
+//. Core Idea of  Boyre-Moore’s Voting Algorithm
+
+//Maintain:
+
+//maj → potential majority candidate
+
+//count → frequency counter
+
+//If count becomes 0, choose a new candidate.
+
+//If current element equals candidate → increment count
+
+////Otherwise → decrement count
+
+//👉 The logic works because the majority element cannot be completely canceled out by other elements.
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -19,3 +37,13 @@ public:
      return maj;
     }
 };
+
+
+
+
+//ye optimal solution hai 
+//We traverse the array once using a single loop.
+
+//Each element is processed in O(1) time.
+
+//👉 Overall Time Complexity = O(n)
