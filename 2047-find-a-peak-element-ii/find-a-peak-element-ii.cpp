39cbaa5cb23a1f96d0b38408d35f3301f0ae487c
Rimjhim Verma <rimjhimv30@gmail.com>
2026-02-26 22:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (36.88%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -12,7 +12,7 @@ public:
             
             int mid = low + (high - low) / 2;
             
-            // Step 1: Find max element row in mid column
+            
             int maxRow = 0;
             for (int i = 0; i < m; i++) {
                 if (mat[i][mid] > mat[maxRow][mid]) {
@@ -20,11 +20,11 @@ public:
                 }
             }
             
-            // Step 2: Check neighbors
+           
             int left  = (mid - 1 >= 0) ? mat[maxRow][mid - 1] : -1;
             int right = (mid + 1 < n) ? mat[maxRow][mid + 1] : -1;
             
-            // Step 3: Check if peak
+            
             if (mat[maxRow][mid] > left && mat[maxRow][mid] > right) {
                 return {maxRow, mid};
             }
