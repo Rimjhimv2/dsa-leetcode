ded37022ce5fa621d4545509cd461292235ebb78
Rimjhimv2 <rimjhimv30@gmail.com>
2026-03-14 14:47:59 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (32.47%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,33 +1,76 @@
+
+
+//ye dfs approch hai
+
+// class Solution {
+// public:
+// //////ab yaha dfs call kro
+//    void dfs(vector<vector<int>>& rooms,int source,vector<bool>&visited){
+//     ////////sabse phle source ko mark karlo
+//     visited[source ]= true;
+//     ///////ab for loop lagao
+
+//     for(int j = 0; j<rooms[source].size();j++){
+//         int node= rooms[source][j];
+//         if(!visited[node]){
+//             ///////dfs call akro
+//             dfs(rooms,node,visited);
+//         }
+
+//     }
+//    }
+//     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+//         ///////room ka size nikalo 
+//         int n = rooms.size();
+//         ///////ab ek visited array banao
+//         vector<bool>visited(n,false);
+//         ///////ab dfs call kro
+//         dfs(rooms,0,visited);
+//         //////ab pure room mai dekho koi ek single room khali toh nahi hai 
+//         for(int i =0;i<n;i++){
+//             if(!visited[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+
+
+//ab 2nd approch bfs
+
 class Solution {
 public:
-//ab yaha dfs call kro
-   void dfs(vector<vector<int>>& rooms,int source,vector<bool>&visited){
-    //sabse phle source ko mark karlo
-    visited[source ]= true;
-    //ab for loop lagao
-
-    for(int j = 0; j<rooms[source].size();j++){
-        int node= rooms[source][j];
-        if(!visited[node]){
-            //dfs call akro
-            dfs(rooms,node,visited);
-        }
-
-    }
-   }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        //room ka size nikalo 
+        //room ka size nikal lo 
         int n = rooms.size();
-        //ab ek visited array banao
-        vector<bool>visited(n,false);
-        //ab dfs call kro
-        dfs(rooms,0,visited);
-        //ab pure room mai dekho koi ek single room khali toh nahi hai 
-        for(int i =0;i<n;i++){
+        //visited array bnao
+
+        vector<int>visited(n,false);
+        queue<int>que;
+        que.push(0);
+        visited[0]=true;
+        while(!que.empty()){
+            int curr = que.front();
+            que.pop();
+            for(int i =0;i<rooms[curr].size();i++){
+
+              int it = rooms[curr][i];
+              if(!visited[it]){
+                visited[it]=true;
+                que.push(it);
+              }
+            }
+
+        }
+        for(int i = 0 ; i<n ;i++){
             if(!visited[i]){
                 return false;
             }
         }
-        return true;
+return true;
+
+
     }
 };
