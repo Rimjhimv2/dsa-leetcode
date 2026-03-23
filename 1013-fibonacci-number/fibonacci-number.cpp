307c0bc69b2c3cc66da87ccfa4cb51cdba408dfc
Rimjhimv2 <rimjhimv30@gmail.com>
2026-03-23 10:14:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.39%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,3 +1,29 @@
+
+// int fibon(int n, vector<int>& dp)
+// {
+//     if(n <= 1)
+//         return n;
+
+//     if(dp[n] != -1)
+//         return dp[n];
+
+//     return dp[n] = fibon(n-1, dp) + fibon(n-2, dp);
+// }
+
+// 👉 Approach:
+
+// ✅ Top-Down DP (Memoization)
+
+// Recursion use hota hai
+// dp[n] me results store hote hain
+// Pehle problem solve → phir store
+
+// int fib(int n) {
+//     vector<int> dp(n+1, -1);
+//     return fibon(n, dp);
+// }
+
+
 class Solution {
 public:
     int fibon(int n , vector<int> &dp){
@@ -12,7 +38,25 @@ public:
     }
     int fib(int n) {
         vector<int>dp(n+1,-1);
-        return fibon(n,dp);
+        // return fibon(n,dp);
+        if(n<= 1 ){
+            return n ; 
+        }
+        dp[0]=0;
+        dp[1]= 1;
+        
+        for(int i = 2;i<=n;i++){
+dp[i]= dp[i-1]+dp[i-2];
+        }
+        return dp[n];
         
     }
 };
+
+// 👉 Approach:
+
+// ✅ Bottom-Up DP (Tabulation)
+
+// No recursion
+// Start from base cases
+// Build answer step by step
