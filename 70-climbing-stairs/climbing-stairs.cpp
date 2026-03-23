eafd49530bc2e916cb2a5e070d8e5ca2447e8f0d
Rimjhimv2 <rimjhimv30@gmail.com>
2026-03-23 12:03:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (33.16%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -23,30 +23,54 @@
 
 //////now by using bottom up approach
 
+
+
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+
+//         // Create DP array of size n+2 to avoid out-of-bounds
+//         // dp[i] = number of ways to reach from step i to step n
+//         vector<int> dp(n+2, 0);
+
+//         // Base cases:
+//         // If we are already at the top, there is 1 way (stay there)
+//         dp[n] = 1;
+
+//         // If we go beyond the top, there are 0 ways
+//         dp[n+1] = 0;
+
+//         // Fill the dp array in reverse (bottom-up approach)
+//         for(int i = n-1; i >= 0; i--) {
+
+//             // At each step, we can take 1 or 2 steps
+//             // So total ways = ways from next step + ways from next to next step
+//             dp[i] = dp[i+1] + dp[i+2];
+//         }
+
+//         // Answer: number of ways to start from step 0
+//         return dp[0];
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int climbStairs(int n) {
+        vector<int>dp(n+2,0);
+        int curr;
+        int next2 = 0;
+        int next  = 1 ;
 
-        // Create DP array of size n+2 to avoid out-of-bounds
-        // dp[i] = number of ways to reach from step i to step n
-        vector<int> dp(n+2, 0);
 
-        // Base cases:
-        // If we are already at the top, there is 1 way (stay there)
-        dp[n] = 1;
-
-        // If we go beyond the top, there are 0 ways
-        dp[n+1] = 0;
-
-        // Fill the dp array in reverse (bottom-up approach)
-        for(int i = n-1; i >= 0; i--) {
-
-            // At each step, we can take 1 or 2 steps
-            // So total ways = ways from next step + ways from next to next step
-            dp[i] = dp[i+1] + dp[i+2];
+        for(int i=n-1;i>=0;i--){
+            curr = next+next2;
+            next2 = next;
+            next = curr;
         }
-
-        // Answer: number of ways to start from step 0
-        return dp[0];
-    }
+        
+        return curr;
+            }
 };
