b715138171c0b06175d9d085482c1943e2f1b0f2
Rimjhim Verma <rimjhimv30@gmail.com>
2026-03-23 22:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (51.06%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -18,22 +18,76 @@
 // };
 
 
+
+
+
+// class Solution {
+//      public:
+//  int ways(int n , vector<int>& cost,vector<int>&dp){
+// if(n<=1){
+//     return 0;
+// }
+// if(dp[n]!= -1){
+//     return dp[n];
+// }
+//  return dp[n]=min(cost[n-1]+ways(n-1,cost,dp),
+//    (cost[n-2]+ways(n-2,cost,dp)));
+//  }
+//    int minCostClimbingStairs(vector<int>& cost) {
+//      int n = cost.size();
+//     vector<int>dp(n+1,-1);
+//     return ways(n,cost,dp);
+    
+//    }
+// };
+
+
+///////bottom up approach se solve kroo
+
+// class Solution {
+//         public:
+ 
+ 
+//    int minCostClimbingStairs(vector<int>& cost) {
+//      int n = cost.size();
+//      vector<int>dp(n+1,0);
+//      dp[0]=0;
+//      dp[1]=0;
+//      //Loop me cost[n-1] kyu nahi le rahe, cost[i-1] kyu lete hain?
+// //🧠 Simple answer:
+// //👉 Kyuki i current stair hai
+// //👉 aur n last stair hai
+//      for(int i = 2;i<=n;i++){
+//         dp[i]=min(cost[i-1]+dp[i-1],
+//   (cost[i-2]+dp[i-2]));
+//  }
+     
+   
+//      return dp[n];
+    
+//    }
+// };
+
+
+//////space optimization
+
 class Solution {
-     public:
- int ways(int n , vector<int>& cost,vector<int>&dp){
-if(n<=1){
-    return 0;
-}
-if(dp[n]!= -1){
-    return dp[n];
-}
- return dp[n]=min(cost[n-1]+ways(n-1,cost,dp),
-   (cost[n-2]+ways(n-2,cost,dp)));
- }
+        public:
+ 
+ 
    int minCostClimbingStairs(vector<int>& cost) {
      int n = cost.size();
-    vector<int>dp(n+1,-1);
-    return ways(n,cost,dp);
+     vector<int>dp(n+1,0);
+     int curr;
+     int prev=0;
+     int prev2 = 0;
+     for(int i = 2;i<=n;i++){
+        curr = min(prev+cost[i-1],
+        prev2+cost[i-2]);
+        prev2 = prev;
+        prev = curr;
+     }
+      return curr;
     
    }
 };
