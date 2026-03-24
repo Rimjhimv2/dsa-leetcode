8ac9c184e216408ffa427e24c21d2a7d561325e6
Rimjhim Verma <rimjhimv30@gmail.com>
2026-03-24 21:09:27 +0530

    Time: 39 ms (47.25%) | Memory: 27.3 MB (48.67%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,46 +1,78 @@
 
 
+// class Solution {
+// public:
+// int n;
+// int t[2501][2501];
+// int solve(vector<vector<int>>& nums,int i ,int P){
+//     ///base case
+//     if(i>=n){
+//         return 0;
+//     }
+// //////❌ Agar tum directly use karo:
+// //t[i][P]
+
+// //Toh jab P = -1:
+
+// //t[i][-1]  ❌ INVALID
+
+// //👉 Crash / undefined behavior
+// //oor t ki limit bhi dekhna 
+//      if (t[i][P + 1] != -1) {   // FIXED
+//             return t[i][P + 1];
+//         }
+//     int take = 0;
+//     /////nums[i][0] > nums[P][1]
+
+// //Break karte hain:
+
+// //nums[i][0] → current pair ka start
+// //nums[P][1] → previous selected pair ka end
+// //or yaha 2d vector diya hai so esliye like hame curr ppair ka start value ko prev pair ke end value se compare karna 
+//     if(P==-1 || nums[i][0]>nums[P][1]){
+//         take = 1+solve(nums,i+1,i);
+//     }
+//     int skip = solve(nums,i+1,P);
+    
+//     return t[i][P+1]=max(take,skip);
+// }
+//        int findLongestChain(vector<vector<int>>& pairs) {
+//       memset(t,-1,sizeof(t));
+//       n = pairs.size() ;
+//       ////////imp point
+//       sort(begin(pairs),end(pairs));
+//       return solve(pairs,0,-1) ;
+//     }
+// };
+
+
+
+/////bottom up approch 
 class Solution {
 public:
-int n;
-int t[2501][2501];
-int solve(vector<vector<int>>& nums,int i ,int P){
-    ///base case
-    if(i>=n){
-        return 0;
-    }
-//////❌ Agar tum directly use karo:
-//t[i][P]
+    int findLongestChain(vector<vector<int>>& nums) {
 
-//Toh jab P = -1:
 
-//t[i][-1]  ❌ INVALID
-
-//👉 Crash / undefined behavior
-//oor t ki limit bhi dekhna 
-     if (t[i][P + 1] != -1) {   // FIXED
-            return t[i][P + 1];
+int n = nums.size();
+sort(begin(nums),end(nums));
+vector<int>t(n,1);
+//sabko phle 1 iniliaze kra kyoki sab khud to subseq honge hi 
+  int maxLis = 1;
+  //1 esliye becoz har element khud mai subsequence hai hi esliye 
+for(int i = 0; i<n;i++){
+    for(int j = 0;j<i;j++){
+        if(nums[i][0]>nums[j][1]){
+            t[i]=max(t[i],t[j]+1);
+            maxLis = max(maxLis,t[i]);
         }
-    int take = 0;
-    /////nums[i][0] > nums[P][1]
-
-//Break karte hain:
-
-//nums[i][0] → current pair ka start
-//nums[P][1] → previous selected pair ka end
-//or yaha 2d vector diya hai so esliye like hame curr ppair ka start value ko prev pair ke end value se compare karna 
-    if(P==-1 || nums[i][0]>nums[P][1]){
-        take = 1+solve(nums,i+1,i);
     }
-    int skip = solve(nums,i+1,P);
-    
-    return t[i][P+1]=max(take,skip);
 }
-       int findLongestChain(vector<vector<int>>& pairs) {
-      memset(t,-1,sizeof(t));
-      n = pairs.size() ;
-      ////////imp point
-      sort(begin(pairs),end(pairs));
-      return solve(pairs,0,-1) ;
-    }
+
+return maxLis;
+//t[i]= lis end at ith index
+//t[j]= j par end hone vala sabse baada subsequence kya hai 
+
+//and t[j] kya hai j index par jake uski length dekhlo for agar value choti hai toh ham ek vaue add karke max dekhenge
+
+  }
 };
