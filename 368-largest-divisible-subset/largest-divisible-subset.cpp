57946e9e7da0f7a61e86911297d17a3c73b47436
Rimjhim Verma <rimjhimv30@gmail.com>
2026-03-27 22:51:19 +0530

    Time: 11 ms (63.17%) | Memory: 12.7 MB (18.75%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,36 +1,68 @@
-class Solution {
-public:
-    vector<int> largestDivisibleSubset(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        
-        vector<int> dp(n, 1);
-        vector<int> parent(n, -1);
-        
-        int maxi = 1;
-        int lastIndex = 0;
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]){
-                    dp[i] = dp[j] + 1;
-                    parent[i] = j;
-                }
+
+// class Solution {
+// public:
+// ///ye lis ka hi approch follow kar raha but yaha hame length nahi balki longest sequence hi nikalna hai 
+// void solve(int idx,vector<int>&nums,vector<int>&result,vector<int>&temp,int prev){
+//     ///base case 
+//     if(idx>= nums.size()){
+//         if(temp.size()>result.size()){
+//             result = temp;
+//         }
+//         return;
+//     }
+//     ///take option
+//     if(prev == -1 || nums[idx] % prev == 0){
+//         temp.push_back(nums[idx]);
+//         solve(idx+1,nums,result,temp,nums[idx]);
+//         temp.pop_back();
+//     }
+//     solve(idx+1,nums,result,temp,prev);
+// }
+//     vector<int> largestDivisibleSubset(vector<int>& nums) {
+//         sort(begin(nums),end(nums));
+//         vector<int>result;
+//         vector<int>temp;
+//         int prev = -1;
+//         solve(0,nums,result,temp,prev);
+//         return result;
+
+//             }
+// };
+
+//most optimized approach i.e bottom up approch
+
+ class Solution {
+ public:
+
+ vector<int> largestDivisibleSubset(vector<int>& nums) {
+    int n = nums.size();
+    sort(begin(nums),end(nums));
+vector<int>t(n,1);
+vector<int>prev_idx(n,-1);
+int maxLen = 1;
+int last_chosen_index = 0;
+for(int i = 1 ; i< n ; i++){
+    for( int j = 0; j<i ; j++){
+       if(nums[i] % nums[j] == 0){
+            if(t[i]< t[j]+1){
+                t[i ] = t[j]+ 1;
+                prev_idx[i] = j;
+
             }
-            
-            if(dp[i] > maxi){
-                maxi = dp[i];
-                lastIndex = i;
+
+            if (t[i ]> maxLen){
+                maxLen = t[i];
+                last_chosen_index = i;
             }
         }
-        
-        vector<int> ans;
-        while(lastIndex != -1){
-            ans.push_back(nums[lastIndex]);
-            lastIndex = parent[lastIndex];
-        }
-        
-        reverse(ans.begin(), ans.end());
-        return ans;
     }
-};
+}
+
+vector<int>result ;
+while(last_chosen_index != -1){
+    result.push_back(nums[last_chosen_index]);
+    last_chosen_index = prev_idx[last_chosen_index];
+}
+return result;
+ }
+ };
