4f2d8f63dfc7fe9806e2583ae5b823b5a49903a2
Rimjhim Verma <rimjhimv30@gmail.com>
2026-03-30 19:41:39 +0530

    Time: 31 ms (10.71%) | Memory: 26.7 MB (88.95%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string s1, string s2) {
+        int m = s1.length();
+    int n = s2.length();
+    
+    vector<vector<int>>t(m+1,vector<int>(n+1));
+    for(int i = 0 ; i<m+1;i++){
+        for(int j = 0 ; j<n+1;j++){
+            if(i ==0 || j==0){
+                t[i][j]= i+j;
+            }
+          else if(s1[i-1] == s2[j-1]){
+                t[i][j]= 1 + t[i-1][j-1];
+            }
+            else{
+                t[i][j]= min(1+t[i][j-1],1+t[i-1][j]);
+            }
+        }
+    }
+    ////ab back tracking karo  tumhe length mil gayi hai 
+    int i = m ,j = n ;
+    string result = "";
+    while( i>0 && j>0){
+        if(s1[i-1]==s2[j-1]){
+            result.push_back(s1[i-1]);
+            i--;
+            j--;
+        }
+        else if(t[i-1][j]<t[i][j-1]){
+            result.push_back(s1[i-1]);
+            i--;
+        }
+        else{
+            result.push_back(s2[j-1]);
+            j--;
+        }
+    }
+    while(i>0){
+        result.push_back(s1[i-1]);
+        i--;
+    }
+     while(j > 0){
+            result.push_back(s2[j-1]);
+            j--;
+        }
+         // Step 4: Reverse result
+        reverse(result.begin(), result.end());
+
+        return result;
+        
+    }
+};
+
+    
+
