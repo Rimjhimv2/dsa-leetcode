a271a0ec5bd5266fd34d526a4b3448fffdc7b8d3
Rimjhim Verma <rimjhimv30@gmail.com>
2026-04-07 23:08:35 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (7.99%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,67 @@
+// class Solution {
+// public:
+//     int n;
+//     int t[201][201];
+
+//     int solve(vector<vector<int>>& triangle, int row, int col) {
+
+//         if (row == n - 1) {
+//             return triangle[row][col];
+//         }
+
+//         if (t[row][col] != -1) {
+//             return t[row][col];
+//         }
+
+//       int minPath = triangle[row][col] + min(solve(triangle,row+1,col),solve(triangle,row+1,col+1));
+
+//         return t[row][col] = minPath;
+//     }
+
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         n = triangle.size();
+//         memset(t, -1, sizeof(t));
+//         return solve(triangle, 0, 0);
+//     }
+// };
+
+
+//intution 
+
+//jab ham last row mai pahuch gaye hai to 
+//to hame us value to return kar rahe 
+
+// min sum nikalo 
+//us value ko add kro or min nikalo 
+
+
+
+//////now method 2 
+//state defination 
+//t[row][col]= min path sum from [row][col] to bottom of the triangle.
+
+
+//ab last row mai kya 
+// jo bhi value hai usko return krdo
+
+//yani t[n-1][col]
+//or ham last se start kar rahe 
+///or last se uper jaynge 
+
+
+class Solution{
+    public:
+
+     int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+
+        vector<vector<int>>t = triangle;
+        for(int row = n-2 ; row>=0;row--){
+            for(int col = 0 ; col<=row ; col++){
+                t[row][col] = t[row][col] + min( t[row+1][col] , t[row+1][col+1]);
+
+            }
+        }
+        return t[0][0];
+}
+};
