a55484c3c58eca0e7ffe5446483759b87d6d5a7c
Rimjhim Verma <rimjhimv30@gmail.com>
2026-06-09 00:14:21 +0530

    Time: 3 ms (45.73%) | Memory: 9.9 MB (10.84%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,10 +1,35 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        sort(begin(s),end(s));
-        sort(begin(t),end(t));
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         sort(begin(s),end(s));
+//         sort(begin(t),end(t));
 
-        return s==t;
+//         return s==t;
         
-    }
-};
+//     }
+// };
+
+class Solution {
+ public:
+    bool isAnagram(string s, string t) {
+        
+//base case
+if(s.length()!= t.length()){
+    return false;
+}
+  unordered_map<char, int> mp;
+  for(int i = 0 ; i<s.length();i++){
+    mp[s[i]]++;
+  }
+  for(int i = 0 ; i<t.length();i++){
+    mp[t[i]]--;
+  
+if (mp[t[i]] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+  }
+  
+   };
