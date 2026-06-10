47535036d4d67edc24f96cb919612ab6b75f1503
Rimjhim Verma <rimjhimv30@gmail.com>
2026-06-10 17:06:54 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (77.22%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -12,6 +12,7 @@ public:
       for(auto&it : mp){
         int freq = it.second;
         if(freq % n != 0 ){
+            //Agar kisi character ki frequency strings ki count se divide nahi hoti, to sab strings equal nahi ban sakti.
             return false;
         }
 
