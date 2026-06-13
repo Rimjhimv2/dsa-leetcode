c3111f5eaa83f9ceb0ad32ca86271936c15c744f
Rimjhim Verma <rimjhimv30@gmail.com>
2026-06-13 13:39:42 +0530

    Time: 4 ms (56.58%) | Memory: 25.5 MB (90.48%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,26 +1,54 @@
+// class Solution {
+// public:
+//     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+//         int n = grid.size();
+//         unordered_map<int, int> mp;
+
+//         for(int i = 0 ; i<n;i++){
+//             for(int j = 0 ; j< n ; j++){
+//                 mp[grid[i][j]]++;
+//             }
+//         }
+
+//         int repeating = -1;
+//         int duplicating = -1;
+//         for(int num = 1 ; num<=n* n ; num++){
+//            if(mp[num]==2){
+//             repeating= num;
+//            }
+//            if(mp[num]==0){
+//             duplicating = num;
+//            }
+//         }
+
+//         return {repeating, duplicating};
+//     }
+// };
+
 class Solution {
 public:
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n = grid.size();
-        unordered_map<int, int> mp;
+  vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
 
-        for(int i = 0 ; i<n;i++){
-            for(int j = 0 ; j< n ; j++){
-                mp[grid[i][j]]++;
-            }
+    int n = grid.size();
+    long long  N = n* n ;
+    long long  gridSum=0;
+    long long  gridsqSum= 0;
+    for(int i = 0 ; i< n ; i++){
+        for(int j = 0 ; j < n ; j++){
+            gridSum += grid[i][j];
+            gridsqSum += grid[i][j]* grid[i][j];
         }
-
-        int repeating = -1;
-        int duplicating = -1;
-        for(int num = 1 ; num<=n* n ; num++){
-           if(mp[num]==2){
-            repeating= num;
-           }
-           if(mp[num]==0){
-            duplicating = num;
-           }
-        }
-
-        return {repeating, duplicating};
     }
+    long long Sum = (N* (N+1))/2;
+    long long sqSum= (N*(N+1)*(2*N+1))/6;
+
+    int sqDiff = gridsqSum - sqSum;
+    int sumDiff = gridSum - Sum ; 
+
+    int a = (sqDiff/sumDiff + sumDiff)/2;
+    int b = (sqDiff/sumDiff - sumDiff)/2;
+
+return { a, b };
+
+  }
 };
