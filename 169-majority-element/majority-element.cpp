05f805540b3c78257388a87e0200f7ff858c22ac
Rimjhim Verma <rimjhimv30@gmail.com>
2026-06-14 00:31:32 +0530

    Time: 3 ms (27.78%) | Memory: 28.1 MB (86.97%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -16,34 +16,62 @@
 
 //👉 The logic works because the majority element cannot be completely canceled out by other elements.
 
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//      int n = nums.size();
+//      int count= 0;
+//      int maj = NULL;
+//      for(int i = 0;i<n;i++){
+//         if(count == 0 ){
+//             count = 1;
+//             maj = nums[i];
+//         }
+//         else if(nums[i]==maj){
+//             count++;
+//         }
+//         else{
+//             count--;
+//         }
+//      }
+//      return maj;
+//     }
+// };
+
+
+
+
+// //ye optimal solution hai 
+// //We traverse the array once using a single loop.
+
+// //Each element is processed in O(1) time.
+
+// //👉 Overall Time Complexity = O(n)
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-     int n = nums.size();
-     int count = 0;
-     int maj = NULL;
-     for(int i = 0;i<n;i++){
-        if(count == 0 ){
-            count = 1;
-            maj = nums[i];
+
+        sort(nums.begin(), nums.end());
+
+        int freq = 1;
+        int maxFreq = 1;
+        int ans = nums[0];
+
+        for(int i = 1; i < nums.size(); i++) {
+
+            if(nums[i] == nums[i-1]) {
+                freq++;
+            } else {
+                freq = 1;
+            }
+
+            if(freq > maxFreq) {
+                maxFreq = freq;
+                ans = nums[i];
+            }
         }
-        else if(nums[i]==maj){
-            count++;
-        }
-        else{
-            count--;
-        }
-     }
-     return maj;
+
+        return ans;
     }
 };
-
-
-
-
-//ye optimal solution hai 
-//We traverse the array once using a single loop.
-
-//Each element is processed in O(1) time.
-
-//👉 Overall Time Complexity = O(n)
