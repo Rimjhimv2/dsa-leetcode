ff1f3c88070676b1032b9323f2d9b1a150e6ac08
Rimjhim Verma <rimjhimv30@gmail.com>
2026-06-16 00:26:17 +0530

    Time: 39 ms (71.61%) | Memory: 45.4 MB (56.29%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,17 +1,43 @@
-class Solution {
-public:
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int count = 0;;
+//         int n = nums.size();
+//         for(int i = 0; i< n; i++){
+//             int sum = 0;
+//             for(int j = i ; j< n;j++){
+//                 sum+= nums[j];
+//                 if(sum == k ){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+
+
+ class Solution {
+ public:
     int subarraySum(vector<int>& nums, int k) {
-        int count = 0;;
-        int n = nums.size();
-        for(int i = 0; i< n; i++){
-            int sum = 0;
-            for(int j = i ; j< n;j++){
-                sum+= nums[j];
-                if(sum == k ){
-                    count++;
-                }
-            }
-        }
-        return count;
+unordered_map<int,int>mp;
+int n = nums.size();
+int cumSum = 0 ; 
+mp.insert({0,1});
+int result = 0 ; 
+for(int i = 0 ; i< n ; i++){
+    cumSum+=nums[i];
+    if(mp.find(cumSum-k)!= mp.end()){
+        result+=mp[cumSum-k];
     }
-};
+
+    
+    mp[cumSum]++;
+
+    
+    }
+   return result;
+
+    }
+ };
