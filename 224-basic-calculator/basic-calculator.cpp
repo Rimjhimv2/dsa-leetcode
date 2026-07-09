7bd613cd8b1283912e202582c41a9648b69cb841
Rimjhim Verma <rimjhimv30@gmail.com>
2026-07-09 17:03:04 +0530

    Time: 6 ms (26.43%) | Memory: 12.6 MB (83.09%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -5,34 +5,35 @@ public:
         stack<long long> st;
 
         long long result = 0;
-        long long num = 0;
-        int sign = 1;
+        long long sign = 1;
 
         for (int i = 0; i < s.size(); i++) {
 
             char ch = s[i];
 
-            // Build multi-digit number
             if (isdigit(ch)) {
-                num = num * 10 + (ch - '0');
+
+                long long num = 0;
+
+                while (i < s.size() && isdigit(s[i])) {
+                    num = num * 10 + (s[i] - '0');
+                    i++;
+                }
+
+                result += sign * num;
+                i--;
             }
 
-            // '+' operator
             else if (ch == '+') {
-                result += sign * num;
-                num = 0;
                 sign = 1;
             }
 
-            // '-' operator
             else if (ch == '-') {
-                result += sign * num;
-                num = 0;
                 sign = -1;
             }
 
-            // Opening parenthesis
             else if (ch == '(') {
+
                 st.push(result);
                 st.push(sign);
 
@@ -40,11 +41,7 @@ public:
                 sign = 1;
             }
 
-            // Closing parenthesis
             else if (ch == ')') {
-
-                result += sign * num;
-                num = 0;
 
                 long long prevSign = st.top();
                 st.pop();
@@ -54,15 +51,7 @@ public:
 
                 result = prevResult + prevSign * result;
             }
-
-            // Ignore spaces
-            else if (ch == ' ') {
-                continue;
-            }
         }
-
-        // Add the last number
-        result += sign * num;
 
         return (int)result;
     }
