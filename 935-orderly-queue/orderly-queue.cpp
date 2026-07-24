29181404368022d879a195acf1407ae35eea591d
Rimjhim Verma <rimjhimv30@gmail.com>
2026-07-24 10:47:24 +0530

    Time: 3 ms (63.69%) | Memory: 13.4 MB (60.95%) - LeetSync

---
+++ 935-orderly-queue/orderly-queue.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+
+        if(k>1){
+            sort(s.begin(),s.end());
+            return s;
+            //sort karke return karo 
+        }
+
+        string result = s ; 
+        int n = s.length();
+        for(int l= 1 ; l<=n-1; l++){
+            string temp = s.substr(l)+ s.substr(0,l);
+
+            result = min(result,temp);
+
+        }
+        return result;
+    }
+};
