84d5d28248476396b5769335732657bb4f76d659
Rimjhim Verma <rimjhimv30@gmail.com>
2026-07-26 00:01:49 +0530

    Time: 419 ms (6.14%) | Memory: 8.1 MB (85.74%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -4,6 +4,7 @@ public:
     bool solve(int i, int j, string &s, string &p) {
 
         // Pattern bhi khatam aur string bhi
+        // chevk karta kya s ka i se end tak ka part and p se j tak ka part match karta hai ya nahi 
         if (i == s.size() && j == p.size())
             return true;
 
@@ -14,9 +15,11 @@ public:
         // Current character match?
         bool firstMatch = (i < s.size() &&
                           (s[i] == p[j] || p[j] == '.'));
+                          //ans true or false mai hi aayega 
 
         // Next character '*' hai?
         if (j + 1 < p.size() && p[j + 1] == '*') {
+            //first character a hai and dusra char * hai esiliye j+1 se start kara ahi 
 
             // 0 occurrence OR 1/more occurrence
             return solve(i, j + 2, s, p) ||
