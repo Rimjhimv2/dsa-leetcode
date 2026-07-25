e237a42a9b3e84fc4246232b92895a803403009d
Rimjhim Verma <rimjhimv30@gmail.com>
2026-07-25 17:25:58 +0530

    Time: 3 ms (52.28%) | Memory: 58.6 MB (37.19%) - LeetSync

---
+++ 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long minimumReplacement(vector<int>& nums) {
+        
+        long long ans = 0 ;
+        int prev = nums.back();
+
+        for(int i = nums.size()-2; i>=0;i--){
+            if(nums[i]<= prev){
+                prev = nums[i];
+            }
+
+            else{
+                int part = (nums[i] + prev - 1 )/prev;
+                ans += part-1;
+                prev = nums[i]/part;
+            }
+        }
+        return ans ;
+    }
+};
+//tumhe kitni replacement karni padegi to make the sorted array 
+//array non decreasung order mai hona chaiye 
+//ager array sorted nahi hai toh usee like 1 2 10 4 here 10>4 split the 10 to make the array sorted.
+
