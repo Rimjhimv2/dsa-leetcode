8e119fbd91b73d7d4b296bf0ac42e7ab04efbaad
Rimjhim Verma <rimjhimv30@gmail.com>
2026-08-05 01:31:16 +0530

    Time: 14 ms (53.57%) | Memory: 18.9 MB (82.91%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -19,51 +19,19 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-
-
-        if(root == NULL){
-            return NULL ;
-        }
-        queue<Node*>q;
-        q.push(root);
-
-        while(!q.empty()){
-            int size = q.size();
-            //jaise hi level change hogi prev re ininialize hoga null par 
-            
-             Node* prev = NULL;
-            for(int i = 0 ; i<size ; i++){
-               
-                Node* curr = q.front();
-
-                q.pop();
-
-                if(prev!=NULL){
-                    prev->next = curr;
-
-                }
-                prev = curr;
-
-                if(curr->left){
-                    q.push(curr->left);
-                }
-
-                if(curr->right){
-                    q.push(curr->right);
-                }
-            }
-
-            prev->next = NULL;
-        }
-
-        return root;
         
+        if(root == NULL){
+            return NULL;
+        }
+        if(root->left ){
+            root->left->next = root->right;
+        }
+        if(root->right && root->next){
+            root->right->next = root->next->left;
+        }
+
+        connect(root->left);
+        connect(root->right);
+        return root;
     }
 };
-
-///har node ka next pointer uske right side wale node ki taraf point karta hai
-
-//ager next node ke right mai koi node nahi hai toh next = NULL hogaaa
-
-///perfect binary tree ke 2 children hota hai and leaf node sare same level par hote hai 
-
