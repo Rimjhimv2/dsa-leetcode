009633476301d23ebe449e53faeeb3f12e09eaa3
Rimjhim Verma <rimjhimv30@gmail.com>
2026-08-09 12:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (27.89%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,21 +1,45 @@
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+        
+//         //last se hi start karna hi 
+//         for(int i = digits.size() - 1 ; i>=0 ;i-- ){
+//             if(digits[i] < 9 ){
+//                 digits[i]++;
+//                 return digits;
+//             }
+
+//             digits[i] = 0 ; 
+
+//             //or ager sari hi gigits 9 hai toh digits ke begin mai 1 add karo 
+            
+//         }
+//         digits.insert(digits.begin() , 1);
+
+//             return digits;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        
-        //last se hi start karna hi 
-        for(int i = digits.size() - 1 ; i>=0 ;i-- ){
-            if(digits[i] < 9 ){
+
+        // Last digit se start karna hai
+        for (int i = digits.size() - 1; i >= 0; i--) {
+
+            // Agar digit 9 nahi hai
+            if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
 
-            digits[i] = 0 ; 
-
-            //or ager sari hi gigits 9 hai toh digits ke begin mai 1 add karo 
-            
+            // Agar digit 9 hai, toh 0 kar do
+            digits[i] = 0;
         }
-        digits.insert(digits.begin() , 1);
 
-            return digits;
+        // Agar saare digits 9 the
+        digits.insert(digits.begin(), 1);
+
+        return digits;
     }
 };
