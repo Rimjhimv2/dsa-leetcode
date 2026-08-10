a808f74fa9f6c9895abea46c6c2282a7df17c3fe
Rimjhim Verma <rimjhimv30@gmail.com>
2026-08-10 11:21:32 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (34.89%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -7,9 +7,17 @@ public:
         for (int x : cardPoints) {
             total += x;
         }
+        ///yaha se total sum nikal hi jata  
+
+       ///hamko k cards uthane hai 
+       //or jo cards nhi uthane hai vo beech j=ke hi continous array mai se honge
 
         // Window size = cards we DON'T take
+
         int windowSize = n - k;
+        //etne size ka subaarya bnao or vo subarray mai se hame nahi uthana hai 
+
+        //ab ek window size tak ka subarray bnao or sum nikal 
 
         int windowSum = 0;
 
