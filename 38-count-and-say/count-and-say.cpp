7044c4b1628a3577a63fa7a6883a1751e02498d6
Rimjhim Verma <rimjhimv30@gmail.com>
2026-08-14 20:54:43 +0530

    Time: 4 ms (45.56%) | Memory: 9.8 MB (81.55%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -3,29 +3,26 @@ public:
     string countAndSay(int n) {
 
         string result = "1";
-        for(int i = 2;i<=n;i++){
-            string temp ="";
-            int j =0;
+        for(int i = 2 ; i<=n ; i++){
+            string temp = "";
+            int j = 0;
             while(j<result.length()){
-                int count = 0;
+                int count = 0 ; 
                 char digit = result[j];
-
-                while(j<result.length() && result[j]==digit){
+                while(j < result.length() && result[j]== digit){
                     count++;
                     j++;
                 }
 
-                 temp += to_string(count);
-            temp+=digit;
-
+                temp+=to_string(count);
+                temp+=digit;
             }
-
-         
-
-            result= temp;
+            result = temp ; 
 
         }
-        return result;
-        
+        return result ; 
+       
+
+
     }
 };
