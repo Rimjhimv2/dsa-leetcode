5214fdb499f95fe230208b42523e8469b28794f5
Rimjhim Verma <rimjhimv30@gmail.com>
2026-08-18 00:54:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (88.12%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,20 +1,72 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         stringstream ss(s);
+//         //ss mai aagye 
+
+//         string token;
+//         string result = "";
+
+//         while(ss>>token){
+//             //token mai aarhe sare word ek ek karke
+
+//             result = token + " " + result;
+//         }
+
+//         result.pop_back();
+
+//         return result ;
+//     }
+// };
+
 class Solution {
 public:
     string reverseWords(string s) {
-        stringstream ss(s);
-        //ss mai aagye 
 
-        string token;
-        string result = "";
+        // Step 1: poori string reverse
+        reverse(s.begin(), s.end());
 
-        while(ss>>token){
-            //token mai aarhe sare word ek ek karke
+        int i = 0;
 
-            result = token + " " + result;
+        // l = current word ka starting point
+        // r = answer mein next position
+        int l = 0, r = 0;
+
+        int n = s.length();
+
+        while (i < n) {
+
+            // Spaces ko skip karo
+            while (i < n && s[i] == ' ') {
+                i++;
+            }
+
+            // Current word ko compact position par copy karo
+            while (i < n && s[i] != ' ') {
+                s[r++] = s[i++];
+            }
+
+            // Agar word mila
+            if (l < r) {
+
+                // Word ko reverse karo
+                reverse(s.begin() + l, s.begin() + r);
+
+                // Words ke beech single space
+                s[r] = ' ';
+                r++;
+
+                // Next word ka starting point
+                l = r;
+            }
         }
 
-        result.pop_back();
+        // Last extra space remove
+        if (r > 0)
+            r--;
 
-        return result ;
+        s.resize(r);
+
+        return s;
     }
 };
