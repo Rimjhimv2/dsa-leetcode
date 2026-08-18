ad09b31150d0f76788e8ad34acc6954a158c0037
Rimjhim Verma <rimjhimv30@gmail.com>
2026-08-18 18:39:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (57.90%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,21 +1,42 @@
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+        
+
+//         string prefix = strs[0];
+//         //flower hai 
+//         //ab hame dekhna hai ki flower bhi baki string ka prefix hia ya nahi 
+//         for(int i = 1 ; i<strs.size();i++
+//         ){
+//            while(strs[i].find(prefix) !=0){
+//             //kya flower word flow ke anfder hai or ager nahi tab tak loop vchalega last ke ek lettrt pop back se 
+//                 prefix.pop_back();
+//            } 
+
+//            if (prefix.empty()) { return ""; }
+//         }
+
+//         return prefix;
+//     }
+// };
+
+
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        
+    
+    for(int col = 0 ; col < strs[0].size(); col++){
+        char  ch = strs[0][col];
 
-        string prefix = strs[0];
-        //flower hai 
-        //ab hame dekhna hai ki flower bhi baki string ka prefix hia ya nahi 
-        for(int i = 1 ; i<strs.size();i++
-        ){
-           while(strs[i].find(prefix) !=0){
-            //kya flower word flow ke anfder hai or ager nahi tab tak loop vchalega last ke ek lettrt pop back se 
-                prefix.pop_back();
-           } 
-
-           if (prefix.empty()) { return ""; }
+        for(int row = 1 ; row < strs.size(); row++){
+            if(col >= strs[row].size() || strs[row][col] != ch){
+                return strs[0].substr(0,col);
+            }
         }
+    }
+    return strs[0];
 
-        return prefix;
+
     }
 };
