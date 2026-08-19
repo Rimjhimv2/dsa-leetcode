c273eb6caa5f4262573458473f8d19966f222f57
Rimjhim Verma <rimjhimv30@gmail.com>
2026-08-19 14:39:43 +0530

    Time: 68 ms (10.18%) | Memory: 31.1 MB (15.30%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,14 +1,52 @@
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-       unordered_map<string, vector<string>>mp;
+//        unordered_map<string, vector<string>>mp;
 
-       for(string word:strs){
-        string key = word;
-        sort(key.begin(),key.end());
+//        for(string word:strs){
+//         string key = word;
+//         sort(key.begin(),key.end());
 
-        mp[key].push_back(word);
+//         mp[key].push_back(word);
+//        }
+//        vector<vector<string>>ans;
+
+//        for(auto &it : mp){
+//         ans.push_back(it.second);
+
+//        }
+//        return ans;
+
+//     }
+// };
+
+
+
+class Solution {
+ public:
+     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>>mp;
+
+        for(string word:strs){
+
+         
+
+         int freq[26]={0};
+         for(char ch : word){
+            freq[ch-'a']++;
+         }
+         string key = "";
+
+         for(int i = 0 ; i<26 ; i++){
+            key+= to_string(freq[i]) + "#";
+         }
+        
+
+
+
+                    mp[key].push_back(word);
        }
        vector<vector<string>>ans;
 
@@ -16,7 +54,7 @@ public:
         ans.push_back(it.second);
 
        }
-       return ans;
+        return ans;
 
-    }
+     }
 };
