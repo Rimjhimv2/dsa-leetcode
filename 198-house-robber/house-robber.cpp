dfcf93251c132e1b1db549eb57d9a0a479508365
Rimjhim Verma <rimjhimv30@gmail.com>
2026-08-20 17:09:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (65.17%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,24 +1,46 @@
-class Solution {
-public: 
-int t[101];
+// class Solution {
+// public: 
+// int t[101];
 
-int solve(vector<int>& nums,int i , int n){
+// int solve(vector<int>& nums,int i , int n){
 
-    if(i>=n)//bahar hi chla gaya 
-    return 0 ; 
-    if(t[i]!=-1){
-        return t[i];
-    }
+//     if(i>=n)//bahar hi chla gaya 
+//     return 0 ; 
+//     if(t[i]!=-1){
+//         return t[i];
+//     }
 
 
-    int steal = nums[i] + solve(nums,i+2,n);
-    int skip = solve(nums,i+1,n);
+//     int steal = nums[i] + solve(nums,i+2,n);
+//     int skip = solve(nums,i+1,n);
 
-    return t[i]=  max(steal,skip);
+//     return t[i]=  max(steal,skip);
+// }
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         memset(t,-1,sizeof(t));
+//         return solve (nums,0, n);
+//     }
+// };
+
+ class Solution {
+ public: 
+
+ int rob(vector<int>& nums) {
+int n = nums.size();
+if(n == 1){
+    return nums[0];
 }
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        memset(t,-1,sizeof(t));
-        return solve (nums,0, n);
-    }
+
+vector<int>dp(n+1);
+dp[0] = nums[0];
+dp[1] = max(nums[0],nums[1]);
+
+for(int i = 2 ; i<n ; i++){
+    dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+}
+
+return dp[n-1];
+       }
 };
+        
