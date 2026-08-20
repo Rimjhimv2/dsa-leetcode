1a5a8736b3f83528bf4562dc0162b93a6f66a7d1
Rimjhim Verma <rimjhimv30@gmail.com>
2026-08-21 01:24:43 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (85.72%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,37 +1,64 @@
-class Solution {
-public:
+// class Solution {
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+        
+//         int m = grid.size();
+//         int n = grid[0].size();
 
-int solve(vector<vector<int>>&grid ,int i , int j , int m , int n, vector<vector<int>> &t ){
-    if(i == m-1 && j == n-1){
+//         vector<vector<int>> t(m, vector<int>(n, 0));
+
+//         t[0][0] = grid[0][0];
+
+//         // First row
+//         for (int col = 1; col < n; col++) {
+//             t[0][col] = grid[0][col] + t[0][col-1];
+//         }
+
+//         // First column
+//         for (int row = 1; row < m; row++) {
+//             t[row][0] = grid[row][0] + t[row-1][0];
+//         }
+
+//         // Remaining cells
+//         for (int i = 1; i < m; i++) {
+//             for (int j = 1; j < n; j++) {
+//                 t[i][j] = grid[i][j] + 
+//                           min(t[i-1][j], t[i][j-1]);
+//             }
+//         }
+
+//         return t[m-1][n-1];
+//     }
+// };
+
+//TC: O(m*n)
+// SC: O(m*n)
+
+
+class Solution {
+ public:
+ int m, n ;
+ int t[201][201];
+ int solve(int i , int j ,vector<vector<int>>& grid ){
+
+    if(i== m-1 && j == n-1){
         return grid[i][j];
     }
-    if(t[i][j] != -1){
+
+    if(i>=m || j>=n){
+        return INT_MAX;
+    }
+
+    if(t[i][j]!=-1){
         return t[i][j];
     }
 
-    if(i == m-1){
-
-        //matlab ham last row mai 
-        //to ham khali right ja sakte hai 
-        return t[i][j]= grid[i][j] + solve(grid,i , j+1,m,n,t);
-
-    }
-
-    if(j==n-1){
-        return  t[i][j] = grid[i][j] + solve(grid , i+1, j , m, n,t );
-    }
-    else{
-        return  t[i][j] = grid[i][j] + min(solve(grid , i+1, j , m, n,t ),solve(grid,i , j+1,m,n,t));
-    }
-
-
-}
-    int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-
-        vector<vector<int>>t(m+1,vector<int>(n+1,-1));
-        return solve(grid , 0 , 0 , m , n,t);
-    }
+    return t[i][j] = grid[i][j] + min(solve(i+1,j,grid), solve(i,j+1,grid));
+ }
+  int minPathSum(vector<vector<int>>& grid) {
+m = grid.size();
+n = grid[0].size();
+   memset(t,-1,sizeof(t));
+   return solve(0,0,grid);
+  }
 };
