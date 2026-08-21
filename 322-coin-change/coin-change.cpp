ef2b86a290fb244e21cf2847112dd8d06f3a111a
Rimjhim Verma <rimjhimv30@gmail.com>
2026-08-22 01:13:40 +0530

    Time: 96 ms (5.28%) | Memory: 26.7 MB (48.05%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,25 +1,58 @@
+// class Solution {
+// public:
+
+// int solve(vector<int>& coins,int i , int amount){
+//     if(amount == 0 ){
+//        return 1;
+//     }
+//     if(i== coins.size() ){
+//         return 0;
+//     }
+//     if(coins[i] > amount){
+//         solve(coins,i+1,amount);
+//     }
+//     int take = solve(coins,i,amount - coins[i]);
+
+//     int skip = solve(coins,i+1, amount);
+
+//     return take + skip ; 
+// }
+//     int coinChange(vector<int>& coins, int amount) {
+//         return solve(coins,0,amount);
+//     }
+// };
+
+
+
 class Solution {
 public:
+    int t[301][10001];
+
+    int solve(vector<int>& coins, int i, int amount) {
+        if (amount == 0)
+            return 0;
+
+        if (i == coins.size())
+            return 1e9;
+
+        if (coins[i] > amount)
+            return solve(coins, i + 1, amount);
+
+        if (t[i][amount] != -1)
+            return t[i][amount];
+
+        int take = 1 + solve(coins, i, amount - coins[i]);
+
+        int skip = solve(coins, i + 1, amount);
+
+        return t[i][amount] = min(take, skip);
+    }
+
     int coinChange(vector<int>& coins, int amount) {
+        memset(t, -1, sizeof(t));
 
-        vector<int> dp(amount + 1, amount + 1);
-        dp[0] = 0;
+        int ans = solve(coins, 0, amount);
 
-        for (int i = 1; i <= amount; i++) {
-
-            for (int j = 0; j < coins.size(); j++) {
-
-                if (coins[j] <= i) {
-
-                    dp[i] = min(dp[i],
-                                dp[i - coins[j]] + 1);
-                }
-            }
-        }
-
-        if (dp[amount] > amount)
-            return -1;
-
-        return dp[amount];
+        return ans == 1e9 ? -1 : ans;
     }
 };
