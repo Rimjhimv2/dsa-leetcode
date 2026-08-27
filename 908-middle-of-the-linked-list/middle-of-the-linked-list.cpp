2add9194c5bdea3950f51358d8ca30c60c2b6571
Rimjhim Verma <rimjhimv30@gmail.com>
2026-08-27 17:20:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (90.95%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -6,25 +6,42 @@
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode* curr = head ;
+//         int len = 0 ;
+//         while(curr!=NULL){
+            
+//             len++;
+//             curr = curr->next;
+//         }
+//         int middle = len/2;
+//         curr = head;
+       
+//             for(int i = 0 ; i< middle ; i++){
+//                 curr= curr->next;
+//             }
+        
+//         return curr;
+//     }
+// };
+
+
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* curr = head ;
-        int len = 0 ;
-        while(curr!=NULL){
-            
-            len++;
-            curr = curr->next;
-        }
-        int middle = len/2;
-        curr = head;
-       
-            for(int i = 0 ; i< middle ; i++){
-                curr= curr->next;
-            }
-        
-        return curr;
-    }
-};
+
+
+ListNode*slow = head;
+ListNode*fast = head;
+
+while(fast!=NULL && fast->next!=NULL){
+    slow = slow->next;
+    fast = fast->next->next;
+}
+return slow;
+             }
+ };
