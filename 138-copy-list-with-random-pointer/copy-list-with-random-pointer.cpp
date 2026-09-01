805acaeeee50278a6b64a70ce0dfa02286dbcb74
Rimjhim Verma <rimjhimv30@gmail.com>
2026-09-02 00:00:40 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (72.53%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,45 +1,66 @@
+
 class Solution {
 public:
+
+    void insertCopyInBetween(Node* head) {
+
+        Node* temp = head;
+
+        while (temp != NULL) {
+
+            Node* copyNode = new Node(temp->val);
+
+            copyNode->next = temp->next;
+            temp->next = copyNode;
+
+            temp = copyNode->next;
+        }
+    }
+
+    void connectRandomPointers(Node* head) {
+
+        Node* temp = head;
+
+        while (temp != NULL) {
+
+            Node* copyNode = temp->next;
+
+            if (temp->random != NULL) {
+                copyNode->random = temp->random->next;
+            }
+
+            temp = temp->next->next;
+        }
+    }
+
+    Node* getDeepCopyList(Node* head) {
+
+        Node* temp = head;
+
+        Node* dummyNode = new Node(-1);
+        Node* res = dummyNode;
+
+        while (temp != NULL) {
+
+            res->next = temp->next;
+            res = res->next;
+
+            temp->next = temp->next->next;
+            temp = temp->next;
+        }
+
+        return dummyNode->next;
+    }
+
     Node* copyRandomList(Node* head) {
 
-        if(head == NULL)
+        if (head == NULL) {
             return NULL;
-
-        unordered_map<Node*, Node*> mp;
-
-        Node* newHead = new Node(head->val);
-
-        mp[head] = newHead;
-
-        Node* oldTemp = head->next;
-        Node* newTemp = newHead;
-
-        // Create copied linked list and store mapping
-        while(oldTemp != NULL) {
-
-            Node* copyNode = new Node(oldTemp->val);
-
-            mp[oldTemp] = copyNode;
-
-            newTemp->next = copyNode;
-
-            oldTemp = oldTemp->next;
-            newTemp = newTemp->next;
         }
 
-        // Set random pointers
-        oldTemp = head;
-        newTemp = newHead;
+        insertCopyInBetween(head);
+        connectRandomPointers(head);
 
-        while(oldTemp != NULL) {
-
-            if(oldTemp->random)
-                newTemp->random = mp[oldTemp->random];
-
-            oldTemp = oldTemp->next;
-            newTemp = newTemp->next;
-        }
-
-        return newHead;
+        return getDeepCopyList(head);
     }
 };
