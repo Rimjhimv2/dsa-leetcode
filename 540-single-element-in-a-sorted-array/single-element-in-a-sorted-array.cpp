e15eb4aefbe2d073163823a5f7f279b604f69245
Rimjhim Verma <rimjhimv30@gmail.com>
2026-09-11 14:22:37 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (38.32%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,23 +1,62 @@
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& arr) {
+//        int n = arr.size();
+//         for(int i = 0 ; i < n ; i++){
+//             if(i == 0 ){
+//                if(arr[i+1] != arr[i]){
+//                  return arr[0];
+//                }
+//             }
+//             else if(i == n-1){
+//                 if(arr[i] != arr[i-1]){
+//                     return arr[n-1];
+//                 }
+//             }
+//             else{
+//                 if(arr[i] != arr[i-1] && arr[i]!=arr[i+1]){
+//                     return arr[i];
+//                 } 
+//             }
+//         } 
+//           return -1;
+
+//     }
+// };
+
+
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int low = 0 ; 
-        int high = nums.size()-1;
-        while(low<high){
-            int mid = low + (high - low)/2;
-           
+    int singleNonDuplicate(vector<int>& arr) {
+      int n = arr.size();
+      if(n == 1){
+        return arr[0];
+      }
+      if(arr[0] != arr[1]){
+        return arr[0];
+      }
+      if(arr[n-1] != arr[n-2]){
+        return arr[n-1];
+      }
 
-            if(mid % 2 == 1){
-                mid--;
-            }
-            if(nums[mid]==nums[mid+1]){
-                low = mid+2;
-            }
-            else{
-                high = mid;
-            }
-        }
+      int low = 1;
+      int high = n-2;
+      while(low <= high){
+   int mid = low + (high - low)/2;
+   if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]){
+    return arr[mid];
+   }
 
-        return nums[low];
+   if((mid% 2 == 1 && arr[mid] == arr[mid-1]) || 
+   (mid% 2 == 0 && arr[mid] == arr[mid+1]) ){
+           low = mid + 1;
+   }
+   else{
+    high = mid - 1;
+   }
+
+      }
+   
+return -1;
     }
-};
+ };
