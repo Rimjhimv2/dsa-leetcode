c7251a41ebbf457930db1a70f638fd0f4b1bb6bd
Rimjhim Verma <rimjhimv30@gmail.com>
2026-09-15 04:01:47 +0530

    Time: 7 ms (8.52%) | Memory: 95.9 MB (18.45%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,80 +1,43 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-             // Hamesha chhoti array par binary search lagao
-        if (nums1.size() > nums2.size())
-            return findMedianSortedArrays(nums2, nums1);
 
-int x = nums1.size();
-int y = nums2.size();
-int low = 0 ; 
-int high = x;
-while(low<= high){
-      int partitionX = low + (high - low) / 2;
-    int partitionY = (x+y+1)/2 - partitionX;
+        int i = 0;
+        int j = 0;
 
-    int maxleftX ;
-    if(partitionX == 0 ){
-maxleftX = INT_MIN;
-    }
-    else{
-        maxleftX = nums1[partitionX - 1];
-    }
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int n = n1 + n2;
 
-    int minrightX;
-    if(partitionX == x ){
-        minrightX = INT_MAX;
-    }
-    else{
-minrightX = nums1[partitionX];
-    }
+        vector<int> arr;
 
-    int maxleftY;
-    if(partitionY == 0 ){
-        maxleftY = INT_MIN;
-    }
-    else{
-        maxleftY = nums2[partitionY- 1];
-    }
-    int minrightY;
-    if(partitionY == y ){
-        minrightY = INT_MAX;
-    }
-    else{
-        minrightY = nums2[partitionY];
-    }
+        while(i < n1 && j < n2) {
 
-
-    //partion hogya ab dekh kya valid hai ya nahi 
-   // Kya partition sahi hai?
-            if (maxleftX <= minrightY &&
-                maxleftY <= minrightX) {
-
-        if((x+y)%2 == 0 ){
-        return (max(maxleftX, maxleftY) +
-        min(minrightX, minrightY)) / 2.0;
-        }
-
-        else{
-            return max(maxleftX, maxleftY);
-        }
-    }
-
-   else if (maxleftX > minrightY) {
-
-                high = partitionX - 1;
+            if(nums1[i] < nums2[j]) {
+                arr.push_back(nums1[i]);
+                i++;
             }
-
-            // Partition ko right shift karo
             else {
-
-                low = partitionX + 1;
+                arr.push_back(nums2[j]);
+                j++;
             }
         }
 
-        return 0.0;
-    
-}
+        while(i < n1) {
+            arr.push_back(nums1[i]);
+            i++;
+        }
 
-    
+        while(j < n2) {
+            arr.push_back(nums2[j]);
+            j++;
+        }
+
+        if(n % 2 == 1) {
+            return arr[n / 2];
+        }
+        else {
+            return (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
+        }
+    }
 };
