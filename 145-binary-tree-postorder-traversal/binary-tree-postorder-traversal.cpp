8581c38aa60366d19d56e7c03fb0bf983873cfc5
Rimjhim Verma <rimjhimv30@gmail.com>
2026-09-17 19:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (46.10%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -9,25 +9,65 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+// void post(TreeNode* root,vector<int>&ans){
+//     if(root==NULL){
+//         return ;
+//     }
+//     post(root->left,ans);
+//     //phle left subtree traverse karo 
+//     post(root->right,ans);
+//     //fir right subtree traverse karo 
+//     ans.push_back(root->val);
+//     //and then root ki value dalde
+
+//     //phle left dekho fir right dekho and fir root ki value add kardo 
+// }
+//     vector<int> postorderTraversal(TreeNode* root) {
+        
+//         vector<int>ans;
+//         post(root,ans);
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-void post(TreeNode* root,vector<int>&ans){
-    if(root==NULL){
-        return ;
-    }
-    post(root->left,ans);
-    //phle left subtree traverse karo 
-    post(root->right,ans);
-    //fir right subtree traverse karo 
-    ans.push_back(root->val);
-    //and then root ki value dalde
+        vector<int> postorderTraversal(TreeNode* root) {
+        // LNR
 
-    //phle left dekho fir right dekho and fir root ki value add kardo 
-}
-    vector<int> postorderTraversal(TreeNode* root) {
-        
-        vector<int>ans;
-        post(root,ans);
+        vector<int> ans;
+
+        if(root == NULL){
+            return ans;
+        }
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+
+
+        while( !st.empty()){
+
+            
+               
+                TreeNode* node = st.top();
+                st.pop();
+
+                ans.push_back(node->val);
+                
+                if(node->left){
+                    st.push(node->left);
+                }
+                if(node->right){
+                    st.push(node->right);
+                }
+             
+            }
+          reverse(ans.begin(), ans.end());
+
         return ans;
     }
 };
