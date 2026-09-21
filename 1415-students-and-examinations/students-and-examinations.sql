d1e3f41dde19c7dbf9868eec01aa20c0e7db7ac0
Rimjhim Verma <rimjhimv30@gmail.com>
2026-09-21 22:38:49 +0530

    Time: 1007 ms (83.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,26 @@
+# Write your MySQL query statement below
+
+SELECT 
+    s.student_id,
+    s.student_name,
+    sub.subject_name,
+    COUNT(e.subject_name) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e
+    ON s.student_id = e.student_id
+    AND sub.subject_name = e.subject_name
+GROUP BY 
+    s.student_id,
+    s.student_name,
+    sub.subject_name
+ORDER BY 
+    s.student_id,
+    sub.subject_name;
+
+
+
+
+#Each student from the Students table takes every course from the Subjects table. 
+# bahut hi imp hai ye matlab har ek student ke pass sare subject honge 
+# so cross join 
