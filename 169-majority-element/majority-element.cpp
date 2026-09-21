87531e02e166948b7f1d7bc687bca9e6954e2a89
Rimjhim Verma <rimjhimv30@gmail.com>
2026-09-21 17:41:15 +0530

    Time: 3 ms (29.28%) | Memory: 41.9 MB (8.03%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,19 +1,47 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        map<int , int> mp;
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         map<int , int> mp;
 
-        for(int i = 0 ; i< n ; i++){
-            mp[nums[i]]++;
+//         for(int i = 0 ; i< n ; i++){
+//             mp[nums[i]]++;
            
 
+//         }
+//        for( auto it : mp){
+//           if(it.second > n/2){
+//             return it.first;
+//           }
+//        }
+//        return -1;
+//     }
+// };\
+
+
+
+ class Solution {
+ public:
+    int majorityElement(vector<int>& nums) {
+     int n = nums.size();
+
+     int count = 0 ; 
+     int candidate = 0 ;
+
+     for( int x : nums){
+        if(count == 0 ){
+            candidate = x;
+                   count = 1;
         }
-       for( auto it : mp){
-          if(it.second > n/2){
-            return it.first;
-          }
-       }
-       return -1;
+
+        else if (  x == candidate ){
+            count++;
+        }
+
+        else{
+            count--;
+        }
+     }
+      return candidate;
     }
-};
+ };
